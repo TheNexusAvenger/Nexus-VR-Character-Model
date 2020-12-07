b8498b603d8669a620e3dd7dc4f86a13fd750b74
TheNexusAvenger <zpcook@googlemail.com>
2020-12-06 22:03:02 -0500

    Added Settings.

---
+++ src/State/Settings.lua
@@ -0,0 +1,107 @@
+--[[
+TheNexusAvenger
+
+Stores settings.
+--]]
+
+local NexusVRCharacterModel = require(script.Parent.Parent)
+local NexusObject = NexusVRCharacterModel:GetResource("NexusInstance.NexusObject")
+local NexusEventCreator = NexusVRCharacterModel:GetResource("NexusInstance.Event.NexusEventCreator")
+
+local Settings = NexusObject:Extend()
+Settings:SetClassName("Settings")
+
+
+
+--[[
+Creates a settings object.
+--]]
+function Settings:__new()
+    self:InitializeSuper()
+
+    --Store the settings.
+    self.Defaults = {}
+    self.Overrides = {}
+    self.SettingsChangeEvents = {}
+end
+
+--[[
+Returns the value of a setting.
+--]]
+function Settings:GetSetting(Setting)
+    --Get the table containing the setting.
+    local Defaults,Overrides = self.Defaults,self.Overrides
+    local SplitSettingNames = string.split(Setting,".")
+    for i = 1,#SplitSettingNames - 1 do
+        Defaults = Defaults[SplitSettingNames[i]] or {}
+        Overrides = Overrides[SplitSettingNames[i]] or {}
+    end
+
+    --Return the value.
+    return Overrides[SplitSettingNames[#SplitSettingNames]] or Defaults[SplitSettingNames[#SplitSettingNames]]
+end
+
+--[[
+Sets the value of a setting.
+--]]
+function Settings:SetSetting(Setting,Value)
+    --Set the setting.
+    local Overrides = self.Overrides
+    local SplitSettingNames = string.split(Setting,".")
+    for i = 1,#SplitSettingNames - 1 do
+        if not Overrides[SplitSettingNames[i]] then
+            Overrides[SplitSettingNames[i]] = {}
+        end
+        Overrides = Overrides[SplitSettingNames[i]]
+    end
+    Overrides[SplitSettingNames[#SplitSettingNames]] = Value
+
+    --Fire the changed signal.
+    local Event = self.SettingsChangeEvents[string.lower(Setting)]
+    if Event then
+        Event:Fire()
+    end
+end
+
+--[[
+Sets all the overrides.
+--]]
+function Settings:SetOverrides(Overrides)
+    --Set the overrides.
+    self.Overrides = Overrides
+
+    --Fire all the event changes.
+    for _,Event in pairs(self.SettingsChangeEvents) do
+        Event:Fire()
+    end
+end
+
+--[[
+Returns a changed signal for a setting.
+--]]
+function Settings:GetSettingsChangedSignal(SettingName)
+    SettingName = string.lower(SettingName)
+
+    --Create the event if none exists.
+    if not self.SettingsChangeEvents[SettingName] then
+        self.SettingsChangeEvents[SettingName] = NexusEventCreator:CreateEvent()
+    end
+
+    --Return the event.
+    return self.SettingsChangeEvents[SettingName]
+end
+
+--[[
+Destroys the settings.
+--]]
+function Settings:Destroy()
+    --Disconnect the settings.
+    for _,Event in pairs(self.SettingsChangeEvents) do
+        Event:Disconnect()
+    end
+    self.SettingsChangeEvents = {}
+end
+
+
+
+return Settings
