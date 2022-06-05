994711d7681537f31e361a5e0a3cbfd46168ba00
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-06-05 12:46:53 -0400

    Replace Detect with Experimental.

--- src/State/DefaultCursorService.lua
+++ src/State/DefaultCursorService.lua
@@ -13,9 +13,7 @@ local RunService = game:GetService("RunService")
 local NexusVRCharacterModel = require(script.Parent.Parent)
 local NexusVRCore = require(ReplicatedStorage:WaitForChild("NexusVRCore"))
 local NexusObject = NexusVRCharacterModel:GetResource("NexusInstance.NexusObject")
-local VRPart = NexusVRCore:GetResource("Interaction.VRPart")
 local VRPointing = NexusVRCore:GetResource("Interaction.VRPointing")
-local VRInputService = NexusVRCharacterModel:GetInstance("State.VRInputService")
 
 local DefaultCursorService = NexusObject:Extend()
 DefaultCursorService:SetClassName("DefaultCursorService")
@@ -29,46 +27,9 @@ function DefaultCursorService:__new()
     self:InitializeSuper()
 
     --Register the default values.
-    self.CursorOptionsList = {"Detect", "Enabled", "Disabled", "Experimental"}
+    self.CursorOptionsList = {"Detect", "Enabled", "Disabled"}
     self.CursorOptions = {
         Detect = function()
-            --Continuously update the pointer.
-            while self.CurrentCursorState == "Detect" do
-                --Determine if the UserGui is visible and being pointed at.
-                --Roblox only uses the right hand, so only the right hand is checked.
-                --TODO: Doesn't detect other UIs, such as BillboardGuis.
-                local PointingAtUserGui = false
-                local Camera = Workspace.CurrentCamera
-                local VRCorePanelParts = Camera:FindFirstChild("VRCorePanelParts")
-                if VRCorePanelParts then
-                    local UserGui = VRCorePanelParts:FindFirstChild("UserGui")
-                    if UserGui then
-                        local VRCFrames = VRInputService:GetVRInputs()
-                        local RightHandCFrame = Camera:GetRenderCFrame() * VRCFrames[Enum.UserCFrame.Head]:Inverse() * VRCFrames[Enum.UserCFrame.RightHand]
-                        local UserGuiVRPart = VRPart.GetInstance(UserGui)
-                        local RaycastPointX, RaycastPointY, _ = UserGuiVRPart:Raycast(RightHandCFrame, Enum.NormalId.Front)
-                        if RaycastPointX >= 0 and RaycastPointX <= 1 and RaycastPointY >= 0 and RaycastPointY <= 1 then
-                            PointingAtUserGui = true
-                        end
-                    end
-                end
-
-                --Update the pointer.
-                pcall(function()
-                    StarterGui:SetCore("VRLaserPointerMode", PointingAtUserGui and "Pointer" or "Disabled")
-                end)
-
-                --Wait to update again.
-                task.wait()
-            end
-        end,
-        Enabled = function()
-            StarterGui:SetCore("VRLaserPointerMode", "Pointer")
-        end,
-        Disabled = function()
-            StarterGui:SetCore("VRLaserPointerMode", "Disabled")
-        end,
-        Experimental = function()
             --Enable the pointer.
             StarterGui:SetCore("VRLaserPointerMode", "Pointer")
             VRPointing.PointersEnabled = false
@@ -93,9 +54,15 @@ function DefaultCursorService:__new()
                 end
             end)
         end,
+        Enabled = function()
+            StarterGui:SetCore("VRLaserPointerMode", "Pointer")
+        end,
+        Disabled = function()
+            StarterGui:SetCore("VRLaserPointerMode", "Disabled")
+        end,
     }
     self.CursorDisabledOptions = {
-        Experimental = function()
+        Detect = function()
             RunService:UnbindFromRenderStep("NexusVRCharacterModel_MoveCursorWorkaround")
             VRPointing.PointersEnabled = true
         end,
