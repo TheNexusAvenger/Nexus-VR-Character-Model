22bc48c7eb7919d47b01fb1d49ae012d045ef927
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-09-29 11:23:31 -0400

    Require button to load chat for new Roblox VR UI.

--- src/UI/View/ChatView.lua
+++ src/UI/View/ChatView.lua
@@ -12,6 +12,8 @@ local UserInputService = game:GetService("UserInputService")
 
 local NexusVRCharacterModel = require(script.Parent.Parent.Parent)
 local NexusInstance = NexusVRCharacterModel:GetResource("NexusInstance.NexusInstance")
+local TextButtonFactory = NexusVRCharacterModel:GetResource("NexusButton.Factory.TextButtonFactory").CreateDefault(Color3.fromRGB(0, 170, 255))
+TextButtonFactory:SetDefault("Theme", "RoundedCorners")
 
 local ChatView = NexusInstance:Extend()
 ChatView:SetClassName("ChatView")
@@ -24,112 +26,109 @@ Creates the chat view.
 function ChatView:__new(View: table): nil
     self:InitializeSuper()
 
-    --Create the loading text.
-    --May be needed if the chat structure changes.
-    local Container = View:GetContainer()
-    local LoadingText = Instance.new("TextLabel")
-    LoadingText.BackgroundTransparency = 1
-    LoadingText.Size = UDim2.new(0.6, 0, 0.2, 0)
-    LoadingText.Position = UDim2.new(0.2, 0, 0.4, 0)
-    LoadingText.Font = Enum.Font.SourceSansBold
-    LoadingText.Text = "Loading..."
-    LoadingText.TextScaled = true
-    LoadingText.TextColor3 = Color3.fromRGB(255, 255, 255)
-    LoadingText.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
-    LoadingText.TextStrokeTransparency = 0
-    LoadingText.Parent = Container
-
-    --Set up the chat.
-    --Done in a task in case it fails or yields.
     task.spawn(function()
+        --Wait for a request to load the chat.
+        --Starting release 544, the chat is loaded automatically.
+        local Container = View:GetContainer()
+        local LoadChatButton, LoadChatText = TextButtonFactory:Create()
+        LoadChatButton.AnchorPoint = Vector2.new(0.5, 0.5)
+        LoadChatButton.Size = UDim2.new(0.6, 0, 0.1, 0)
+        LoadChatButton.Position = UDim2.new(0.5, 0, 0.5, 0)
+        LoadChatButton.SizeConstraint = Enum.SizeConstraint.RelativeYY
+        LoadChatButton.Parent = Container
+        LoadChatText.Text = "Load Chat"
+        LoadChatButton.MouseButton1Click:Wait()
+        LoadChatButton:Destroy()
+
         --Load the chat.
         --Taken from the main script.
         local Chat = require(Players.LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("ChatScript"):WaitForChild("ChatMain"))
         local ClientChatModules = game:GetService("Chat"):WaitForChild("ClientChatModules")
         local ChatSettings = require(ClientChatModules:WaitForChild("ChatSettings"))
+        if not Players.LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("Chat") then
+            local containerTable = {}
+            containerTable.ChatWindow = {}
+            containerTable.SetCore = {}
+            containerTable.GetCore = {}
 
-        local containerTable = {}
-        containerTable.ChatWindow = {}
-        containerTable.SetCore = {}
-        containerTable.GetCore = {}
+            containerTable.ChatWindow.ChatTypes = {}
+            containerTable.ChatWindow.ChatTypes.BubbleChatEnabled = ChatSettings.BubbleChatEnabled
+            containerTable.ChatWindow.ChatTypes.ClassicChatEnabled = ChatSettings.ClassicChatEnabled
 
-        containerTable.ChatWindow.ChatTypes = {}
-        containerTable.ChatWindow.ChatTypes.BubbleChatEnabled = ChatSettings.BubbleChatEnabled
-        containerTable.ChatWindow.ChatTypes.ClassicChatEnabled = ChatSettings.ClassicChatEnabled
+            --// Connection functions
+            local function ConnectEvent(name)
+                local event = Instance.new("BindableEvent")
+                event.Name = name
+                containerTable.ChatWindow[name] = event
 
-        --// Connection functions
-        local function ConnectEvent(name)
-            local event = Instance.new("BindableEvent")
-            event.Name = name
-            containerTable.ChatWindow[name] = event
+                event.Event:connect(function(...) Chat[name](Chat, ...) end)
+            end
 
-            event.Event:connect(function(...) Chat[name](Chat, ...) end)
+            local function ConnectFunction(name)
+                local func = Instance.new("BindableFunction")
+                func.Name = name
+                containerTable.ChatWindow[name] = func
+
+                func.OnInvoke = function(...) return Chat[name](Chat, ...) end
+            end
+
+            local function ReverseConnectEvent(name)
+                local event = Instance.new("BindableEvent")
+                event.Name = name
+                containerTable.ChatWindow[name] = event
+
+                Chat[name]:connect(function(...) event:Fire(...) end)
+            end
+
+            local function ConnectSignal(name)
+                local event = Instance.new("BindableEvent")
+                event.Name = name
+                containerTable.ChatWindow[name] = event
+
+                event.Event:connect(function(...) Chat[name]:fire(...) end)
+            end
+
+            local function ConnectSetCore(name)
+                local event = Instance.new("BindableEvent")
+                event.Name = name
+                containerTable.SetCore[name] = event
+
+                event.Event:connect(function(...) Chat[name.."Event"]:fire(...) end)
+            end
+
+            local function ConnectGetCore(name)
+                local func = Instance.new("BindableFunction")
+                func.Name = name
+                containerTable.GetCore[name] = func
+
+                func.OnInvoke = function(...) return Chat["f"..name](...) end
+            end
+
+            --// Do connections
+            ConnectEvent("ToggleVisibility")
+            ConnectEvent("SetVisible")
+            ConnectEvent("FocusChatBar")
+            ConnectEvent("EnterWhisperState")
+            ConnectFunction("GetVisibility")
+            ConnectFunction("GetMessageCount")
+            ConnectEvent("TopbarEnabledChanged")
+            ConnectFunction("IsFocused")
+
+            ReverseConnectEvent("ChatBarFocusChanged")
+            ReverseConnectEvent("VisibilityStateChanged")
+            ReverseConnectEvent("MessagesChanged")
+            ReverseConnectEvent("MessagePosted")
+
+            ConnectSignal("CoreGuiEnabled")
+
+            ConnectSetCore("ChatMakeSystemMessage")
+            ConnectSetCore("ChatWindowPosition")
+            ConnectSetCore("ChatWindowSize")
+            ConnectGetCore("ChatWindowPosition")
+            ConnectGetCore("ChatWindowSize")
+            ConnectSetCore("ChatBarDisabled")
+            ConnectGetCore("ChatBarDisabled")
         end
-
-        local function ConnectFunction(name)
-            local func = Instance.new("BindableFunction")
-            func.Name = name
-            containerTable.ChatWindow[name] = func
-
-            func.OnInvoke = function(...) return Chat[name](Chat, ...) end
-        end
-
-        local function ReverseConnectEvent(name)
-            local event = Instance.new("BindableEvent")
-            event.Name = name
-            containerTable.ChatWindow[name] = event
-
-            Chat[name]:connect(function(...) event:Fire(...) end)
-        end
-
-        local function ConnectSignal(name)
-            local event = Instance.new("BindableEvent")
-            event.Name = name
-            containerTable.ChatWindow[name] = event
-
-            event.Event:connect(function(...) Chat[name]:fire(...) end)
-        end
-
-        local function ConnectSetCore(name)
-            local event = Instance.new("BindableEvent")
-            event.Name = name
-            containerTable.SetCore[name] = event
-
-            event.Event:connect(function(...) Chat[name.."Event"]:fire(...) end)
-        end
-
-        local function ConnectGetCore(name)
-            local func = Instance.new("BindableFunction")
-            func.Name = name
-            containerTable.GetCore[name] = func
-
-            func.OnInvoke = function(...) return Chat["f"..name](...) end
-        end
-
-        --// Do connections
-        ConnectEvent("ToggleVisibility")
-        ConnectEvent("SetVisible")
-        ConnectEvent("FocusChatBar")
-        ConnectEvent("EnterWhisperState")
-        ConnectFunction("GetVisibility")
-        ConnectFunction("GetMessageCount")
-        ConnectEvent("TopbarEnabledChanged")
-        ConnectFunction("IsFocused")
-
-        ReverseConnectEvent("ChatBarFocusChanged")
-        ReverseConnectEvent("VisibilityStateChanged")
-        ReverseConnectEvent("MessagesChanged")
-        ReverseConnectEvent("MessagePosted")
-
-        ConnectSignal("CoreGuiEnabled")
-
-        ConnectSetCore("ChatMakeSystemMessage")
-        ConnectSetCore("ChatWindowPosition")
-        ConnectSetCore("ChatWindowSize")
-        ConnectGetCore("ChatWindowPosition")
-        ConnectGetCore("ChatWindowSize")
-        ConnectSetCore("ChatBarDisabled")
-        ConnectGetCore("ChatBarDisabled")
 
         --Move the chat GUI to the container.
         local ChatWindow = Players.LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Chat")
@@ -137,9 +136,6 @@ function ChatView:__new(View: table): nil
         local ChatFrame = ChatWindow:FindFirstChildOfClass("Frame")
         ChatFrame.Size = UDim2.new(1, 0, 1, 0)
         ChatFrame.Parent = Container
-
-        --Destroy the loading text.
-        LoadingText:Destroy()
 
         --Connect starting chat.
         --For some reason, it is not done when VR is enabled.
